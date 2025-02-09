860f6e3b768725de9f71d3c0dfbda309ab499f14
Joe J <51897213+tfsJoe@users.noreply.github.com>
2025-02-09 00:37:28 -0500

    cycle colors on internal light

--- examples/get-started/blink/main/blink_example_main.c
+++ examples/get-started/blink/main/blink_example_main.c
@@ -1,111 +1,63 @@
-/* Blink Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
 #include "esp_log.h"
-#include "led_strip.h"
 #include "sdkconfig.h"
+#include "led_strip.h"
+#include "driver/rmt_tx.h"
+#include "esp_err.h"
 
-static const char *TAG = "example";
+#define LED_STRIP_GPIO 8  // GPIO8 is the onboard LED on ESP32-C6
+#define LED_STRIP_RESOLUTION_HZ 10000000  // 10MHz resolution
+#define NUM_LEDS 1  // Only 1 LED onboard
 
-/* Use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
-   or you can edit the following line and set a number here.
-*/
-#define BLINK_GPIO CONFIG_BLINK_GPIO
+#define EXT_LED_GPIO 5
 
-static uint8_t s_led_state = 0;
+led_strip_handle_t led_strip;
 
-#ifdef CONFIG_BLINK_LED_STRIP
-
-static led_strip_handle_t led_strip;
-
-static void blink_led(void)
+// Function to initialize the onboard LED
+void setup_led()
 {
-    /* If the addressable LED is enabled */
-    if (s_led_state) {
-        /* Set the LED pixel using RGB from 0 (0%) to 255 (100%) for each color */
-        led_strip_set_pixel(led_strip, 0, 16, 16, 16);
-        /* Refresh the strip to send data */
-        led_strip_refresh(led_strip);
-    } else {
-        /* Set all LED off to clear all pixels */
-        led_strip_clear(led_strip);
-    }
-}
-
-static void configure_led(void)
-{
-    ESP_LOGI(TAG, "Example configured to blink addressable LED!");
-    /* LED strip initialization with the GPIO and pixels number*/
     led_strip_config_t strip_config = {
-        .strip_gpio_num = BLINK_GPIO,
-        .max_leds = 1, // at least one LED on board
+        .strip_gpio_num = LED_STRIP_GPIO,
+        .max_leds = NUM_LEDS,  
     };
-#if CONFIG_BLINK_LED_STRIP_BACKEND_RMT
+
     led_strip_rmt_config_t rmt_config = {
-        .resolution_hz = 10 * 1000 * 1000, // 10MHz
-        .flags.with_dma = false,
+        .resolution_hz = LED_STRIP_RESOLUTION_HZ,
     };
+
     ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
-#elif CONFIG_BLINK_LED_STRIP_BACKEND_SPI
-    led_strip_spi_config_t spi_config = {
-        .spi_bus = SPI2_HOST,
-        .flags.with_dma = true,
-    };
-    ESP_ERROR_CHECK(led_strip_new_spi_device(&strip_config, &spi_config, &led_strip));
-#else
-#error "unsupported LED strip backend"
-#endif
-    /* Set all LED off to clear all pixels */
-    led_strip_clear(led_strip);
 }
 
-#elif CONFIG_BLINK_LED_GPIO
-
-static void blink_led(void)
+// Function to set LED color
+void set_led_color(uint8_t red, uint8_t green, uint8_t blue)
 {
-    /* Set the GPIO level according to the state (LOW or HIGH)*/
-    gpio_set_level(BLINK_GPIO, s_led_state);
-
-    gpio_dump_io_configuration(stdout, (1ULL << BLINK_GPIO));
-    ESP_LOGI(TAG, "\n");
+    led_strip_set_pixel(led_strip, 0, red, green, blue);
+    led_strip_refresh(led_strip);
 }
 
-static void configure_led(void)
+
+// Main function to cycle colors
+void app_main()
 {
-    ESP_LOGI(TAG, "Example configured to blink GPIO LED!");
-    gpio_reset_pin(BLINK_GPIO);
-    gpio_set_pull_mode(BLINK_GPIO, GPIO_FLOATING);
-    gpio_set_drive_capability(BLINK_GPIO, GPIO_DRIVE_CAP_2);
-    /* Set the GPIO as a push/pull output */
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-}
+    setup_led();
 
-#else
-#error "unsupported LED type"
-#endif
-
-void app_main(void)
-{
-
-    /* Configure the peripheral according to the LED type */
-    configure_led();
-
-    gpio_dump_io_configuration(stdout, SOC_GPIO_VALID_GPIO_MASK);
-
-    while (1) {
-        ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
-        blink_led();
-        /* Toggle the LED state */
-        s_led_state = !s_led_state;
-        vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
+    while (1)
+    {
+        // Cycle Red → Green → Blue with fading effect
+        for (int i = 0; i < 256; i += 10) {
+            set_led_color(i, 0, 0);  // Red increasing
+            vTaskDelay(50 / portTICK_PERIOD_MS);
+        }
+        for (int i = 0; i < 256; i += 10) {
+            set_led_color(0, i, 0);  // Green increasing
+            vTaskDelay(50 / portTICK_PERIOD_MS);
+        }
+        for (int i = 0; i < 256; i += 10) {
+            set_led_color(0, 0, i);  // Blue increasing
+            vTaskDelay(50 / portTICK_PERIOD_MS);
+        }
     }
 }
